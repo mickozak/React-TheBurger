2bb286d23a9c33786b9b1e0533d503cfc7bf0f01
M.Kozak <kozak_m@outlook.com>
2018-12-28 22:56:40 +0100

    Updates

---
+++ precache-manifest.cd28389438f2202f7aaa10c733d646a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "50c483b16773d7ec3a6f",
+    "url": "/React-TheBurger/static/css/main.88aa9686.chunk.css"
+  },
+  {
+    "revision": "50c483b16773d7ec3a6f",
+    "url": "/React-TheBurger/static/js/main.50c483b1.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/React-TheBurger/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "8f0a87f4b08328d96396",
+    "url": "/React-TheBurger/static/js/runtime~main.8f0a87f4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-TheBurger/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "65d6c0984f25a8aa4018bc33741ce241",
+    "url": "/React-TheBurger/index.html"
+  }
+];
