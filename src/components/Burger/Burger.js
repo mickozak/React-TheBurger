21ea49c7c8223d51baeaed254a2b4de1b13f5564
M.Kozak <kozak_m@outlook.com>
2018-12-30 16:57:35 +0100

    calcTheIngredientsSumDynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,11 +5,28 @@ import classes from './Burger.css'
 import BurgerIngredient from './BurgerIngrediens/BurgerIngredient'
 
 const burger = (props) =>{
+    //Transformuje obiekt na tablicę
+    let transformIngredients = Object.keys(props.ingredients)
+    //Dodaje wartość - wartość
+    .map(igKey=>{
+        return [...Array(props.ingredients[igKey])]
+        //Zmienia undefined
+        .map((_,i)=>{
+            return <BurgerIngredient key={igKey+i} type={igKey}/>
+        })
+    })
+    //Redukuje elementy tablicy
+    .reduce((preValue,currentValue)=>{
+        return preValue.concat(currentValue)
+    }, [])
+    console.log(transformIngredients)
+    if(transformIngredients.length===0){
+        transformIngredients = <p>Please start adding ingredients!</p>
+    }
     return(
         <div className={classes.Burger}>
             <BurgerIngredient type='bread-top'/>
-            <BurgerIngredient type='cheese'/>
-            <BurgerIngredient type='meat'/>
+            {transformIngredients}
             <BurgerIngredient type='bread-bottom'/>
         </div>
     )
