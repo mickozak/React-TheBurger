56a234cd6c29ee25b33af34a20256d5633edf3c6
M.Kozak <kozak_m@outlook.com>
2018-12-30 14:06:40 +0100

    creatingALayoutComponent

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 
-import classes from './App.css'
+import './App.css';
+
+import Layout from './components/Layout/Layout'
 
 class App extends Component {
   render() {
     return (
-      <div className={classes.Extra}>
-        In progress...
+      <div>
+        <Layout>
+          <p>Test</p>
+        </Layout>
       </div>
     );
   }
