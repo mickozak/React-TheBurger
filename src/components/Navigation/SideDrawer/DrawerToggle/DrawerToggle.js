a43c68066a78c9ca31a6a2892f4d7ef685bf3f25
M.Kozak <kozak_m@outlook.com>
2019-01-06 17:24:50 +0100

    menuIcon

--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,8 +1,12 @@
 import React from 'react'
 
+import classes from './DrawerToggle.css'
+
 const DrawerToggle = (props) => (
-    <div onClick={props.clicked}>
-        Menu
+    <div className={classes.DrawerToggle} onClick={props.clicked}>
+        <div></div>
+        <div></div>
+        <div></div>
     </div>
 )
 
