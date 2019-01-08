4b39fea995086a6175e50622e8c76a3ff5c9de45
M.Kozak <kozak_m@outlook.com>
2019-01-08 20:31:14 +0100

    sendingAPostRequest

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,6 +6,8 @@ import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal'
 import OrderSummary from '../../components/OrderSummary/OrderSummary'
 
+import axios from '../../axios-orders'
+
 const INGREDIENT_PRICES = {
     salad: 0.5,
     cheese: 0.4,
@@ -83,8 +85,26 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        alert('You Continue!')
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            custmore: {
+                name: 'Michal',
+                surname: 'Kozak',
+                adress: {
+                    street: 'Krakowskie Przedmieście 115',
+                    zipCode: '20-485',
+                    country: 'Poland'
+                },
+                email: 'kozak_m@outlook.com'
+            },
+            deliveryMethod: 'fastest'
+        }
+        axios.post('/orders.json',order)
+        .then(response=>console.log(response))
+        .catch(error=>console.log(error))
     }
+    
 
     render () {
         const disabledInfo = {
