99ee30126ee02431dab089d82534735835b1cc25
M.Kozak <kozak_m@outlook.com>
2019-01-14 15:55:29 +0100

    orderSubmission&PassingDataBetweenPages

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -103,43 +103,13 @@ class BurgerBuilder extends Component {
     for(let i in this.state.ingredients){
       queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]))
     }
+    queryParams.push('price='+this.state.totalPrice)
     const queryString = queryParams.join('&')
     this.props.history.push({
       pathname: '/checkout',
       search: '?' + queryString
     })
-    /*this.setState({
-      loading: true
-    });
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      custmore: {
-        name: "Michal",
-        surname: "Kozak",
-        adress: {
-          street: "Krakowskie Przedmieście 115",
-          zipCode: "20-485",
-          country: "Poland"
-        },
-        email: "kozak_m@outlook.com"
-      },
-      deliveryMethod: "fastest"
-    };
-    axios
-      .post("/orders.json", order)
-      .then(response =>
-        this.setState({
-          loading: false,
-          purchasing: false
-        })
-      )
-      .catch(error =>
-        this.setState({
-          loading: false,
-          purchasing: false
-        })
-      );*/
+    
   };
 
   render() {
