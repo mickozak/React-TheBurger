3ed668a2d999481358479869a2848ccd951c07bd
M.Kozak <kozak_m@outlook.com>
2019-01-18 11:36:28 +0100

    validationTwo

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -43,11 +43,11 @@ class ContactData extends Component {
                 },
                 value: '',
                 validation: {
-                    required: true
+                    required: true,
+                    minLength: 5,
+                    maxLength: 5
                 },
                 valid: false,
-                minLength: 5,
-                maxLength: 5
             },
             country: {
                 elementType: 'input',
@@ -110,15 +110,18 @@ class ContactData extends Component {
     }
 
     checkValidity = (value, rules) => {
-        let isValid = false
-        if(rules.required){
-            isValid = value.trim() !== '';
+        let isValid = true;
+        
+        if (rules.required) {
+            isValid = value.trim() !== '' && isValid;
         }
-        if(rules.minLength){
-            isValid = value.lenght >= rules.minLength
+
+        if (rules.minLength) {
+            isValid = value.length >= rules.minLength && isValid
         }
-        if(rules.maxLength){
-            isValid = value.lenght <= rules.maxLength
+
+        if (rules.maxLength) {
+            isValid = value.length <= rules.maxLength && isValid
         }
         return isValid
     }
