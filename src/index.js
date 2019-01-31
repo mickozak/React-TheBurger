d8ab0feacaecc9ac52a85c101a495d2df00e6875
M.Kozak <kozak_m@outlook.com>
2019-01-31 19:47:55 +0100

    addDevtools

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import {Provider} from 'react-redux'
 
 import { BrowserRouter } from 'react-router-dom'
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 const app = (
     <Provider store={store}><BrowserRouter><App/></BrowserRouter></Provider>
