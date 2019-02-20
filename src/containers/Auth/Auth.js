d5ebbe4eb32ccd7052023c7e3c8581d8016d7cb1
M.Kozak <kozak_m@outlook.com>
2019-02-20 19:59:55 +0100

    addingASpinner

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -3,6 +3,7 @@ import Input from '../../components/UI/Input/Input'
 import Button from '../../components/UI/Button/Button'
 import classes from './Auth.css'
 import * as actions from '../../store/actions/index'
+import Spinner from '../../components/UI/Spinner/Spinner'
 
 import { connect } from 'react-redux'
 import { runInThisContext } from 'vm';
@@ -96,7 +97,7 @@ class Auth extends Component{
             })
         }
 
-        const form = formElementsAray.map(formElement=>(
+        let form = formElementsAray.map(formElement=>(
             <Input
                 key={formElement.id}
                 elementType={formElement.config.elementType}
@@ -110,8 +111,19 @@ class Auth extends Component{
             
         ))
 
+        if(this.props.loading) {
+            form = <Spinner/>
+        } 
+
+        let errorMessage = null
+
+        if(this.props.error){
+            errorMessage = (<p>{this.props.error.message}</p>)
+        }
+
         return(
             <div className={classes.Auth}>
+                {errorMessage}
                 <form onSubmit={this.submitHandler}>
                     {form}
                     <Button btnType="Success">SUBMIT</Button>
@@ -124,10 +136,17 @@ class Auth extends Component{
     }
 }
 
+const mapStateToProps = state => {
+    return{
+        loading: state.auth.loading,
+        error: state.auth.error
+    }
+}
+
 const mapDispatchToProps = dispatch => {
     return{
         onAuth: (email,password, isSignup) => dispatch(actions.auth(email,password, isSignup))
     }
 }
 
-export default connect(null,mapDispatchToProps)(Auth)
+export default connect(mapStateToProps,mapDispatchToProps)(Auth)
