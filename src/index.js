5f0788e9a907ee3b60eca008f4097099b24da222
M.Kozak <kozak_m@outlook.com>
2019-02-20 19:42:01 +0100

    storingTheToken

--- src/index.js
+++ src/index.js
@@ -11,12 +11,14 @@ import thunk from 'redux-thunk'
 import { BrowserRouter } from 'react-router-dom'
 
 import orderReducer from './store/reducers/order'
+import authReducer from './store/reducers/auth'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const rootReducer = combineReducers({
     burgerBuilder: burgerBuilderReducer,
-    order: orderReducer
+    order: orderReducer,
+    auth: authReducer
 })
 
 const store = createStore(rootReducer, composeEnhancers(
