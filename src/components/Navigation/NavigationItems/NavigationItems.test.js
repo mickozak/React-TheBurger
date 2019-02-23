360e1039c496b709c5ab9fcdf8318d0c35b10487
M.Kozak <kozak_m@outlook.com>
2019-02-23 19:03:55 +0100

    secondTest

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -7,10 +7,21 @@ import NavigationItems from './NavigationItems'
 import NavigationItem from './NavigationItem/NavigationItem'
 
 configure({adapter: new Adapter()})
+let wrapper
+beforeEach(()=>{
+    wrapper = shallow(<NavigationItems/>)
+})
 
 describe('<NavigationItems/>',()=>{
-    it('should render two <NavigationItem/> elements if of authenticated',()=>{
-        const wrapper = shallow(<NavigationItems/>)
+    it('should render two <NavigationItem/> elements if not authenticated',()=>{
         expect(wrapper.find(NavigationItem)).toHaveLength(2)
+    })
+
+    it('should render three <NavigationItem/> elements if of authenticated',()=>{
+        //wrapper = shallow(<NavigationItems isAuthenticated/>)
+        wrapper.setProps({
+            isAuthenticated: true
+        })
+        expect(wrapper.find(NavigationItem)).toHaveLength(3)
     })
 })
